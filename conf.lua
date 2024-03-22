b78283758e1ebebcf4a91c381184d7c14e8590d4
Sam Hedges <sam.hedges.sh@gmail.com>
2024-03-22 23:32:49 +0000

    Initial commit

---
+++ conf.lua
@@ -0,0 +1,14 @@
+_RELEASE_MODE = false
+_DEMO_MODE = false
+
+function love.conf(t)
+    os.execute("set LOVE_GRAPHICS_USE_OPENGLES=1")
+
+    t.console = not _RELEASE_MODE
+    t.title = 'Card Game'
+    t.version = '11.5'
+    t.window.width = 0
+    t.window.height = 0
+    t.window.minwidth = 100
+    t.window.minheight = 100
+end
